57d23f1e4dd99f24d401b40c374337733ec76737
Saskuatch <asmithamongus@gmail.com>
2020-07-16 20:33:17 -0700

    Added icons, in working order with main app

--- content.js
+++ content.js
@@ -1,6 +1,11 @@
 function appendButton() {
-    const button = `<a class="btnv6_blue_hoverfade btn_medium" href="#" style="margin: 5px;"><span>Add to LootTracker</span></a>`
+    const url = window.location.href
+    console.log(url);
+    array = url.split("/");
+    console.log(array[4]);
+    const button = `<a id="lootTracker" class="btnv6_blue_hoverfade btn_medium" href="http://127.0.0.1:3000/addGame/${array[4]}" style="margin: 5px;"><span>Add to LootTracker</span></a>`
     $("#category_block").append(button);
 }
+
 
 appendButton();
