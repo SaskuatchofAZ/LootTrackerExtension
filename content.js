0b57c5fde36f34d5d6bff1e9e6c7326e89ef548c
Saskuatch <asmithamongus@gmail.com>
2020-07-18 10:10:38 -0700

    changed button href value to live app url

--- content.js
+++ content.js
@@ -3,7 +3,7 @@ function appendButton() {
     console.log(url);
     array = url.split("/");
     console.log(array[4]);
-    const button = `<a id="lootTracker" class="btnv6_blue_hoverfade btn_medium" href="http://127.0.0.1:3000/addGame/${array[4]}" style="margin: 5px;"><span>Add to LootTracker</span></a>`
+    const button = `<a id="lootTracker" class="btnv6_blue_hoverfade btn_medium" href="https://loot-trackerz.herokuapp.com/addGame/${array[4]}" style="margin: 5px;"><span>Add to LootTracker</span></a>`
     $("#category_block").append(button);
 }
 
